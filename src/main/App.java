d6d821da6032b07eaf8efd8dbeed965b8c44f267
Stevem123 <stevenchimbo1@gmal.com>
2024-07-24 17:04:18 -0500

    Examen

--- src/main/App.java
+++ src/main/App.java
@@ -1,7 +1,10 @@
 package main;
 
+import controllers.Ejercicios;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Ejercicios ejercicio = new Ejercicios();
     }
 }
